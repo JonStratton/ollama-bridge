832395e5352c9598a1b68b76683a3f9191ae8aca
JonStratton <jgstratt@sdf.org>
2024-01-24 21:35:02 -0600

    Added chat history, so AI has view of what was prev said.

--- ollama-bridge-xmpp.py
+++ ollama-bridge-xmpp.py
@@ -6,6 +6,7 @@ import argparse, configparser, requests, slixmpp
 class OllamaBridge(slixmpp.ClientXMPP):
    ollama_url = ''
    ollama_model = ''
+   user_messages = {}
    def __init__(self, config):
       self.ollama_url = config['ollama_url'];
       self.ollama_model = config['ollama_model'];
@@ -19,10 +20,17 @@ class OllamaBridge(slixmpp.ClientXMPP):
 
    def message(self, msg):
       if msg['type'] in ('chat', 'normal'):
-         ollama_req = {'model': self.ollama_model, 'prompt': msg['body'], 'stream': False}
+         user = msg['from']
+         if not self.user_messages.get(user):
+            self.user_messages[user] = []
+
+         self.user_messages[user].append({"role": "user", "content": msg['body']})
+         ollama_req = {'model': self.ollama_model, 'stream': False, 'messages': self.user_messages[user]}
          res = requests.post(self.ollama_url, json = ollama_req)
          if res.status_code == requests.codes.ok:
-            msg.reply(res.json()['response']).send()
+            response = res.json()['message']['content']
+            msg.reply(response).send()
+            self.user_messages[user].append({"role": "assistant", "content": response})
 
 
 if __name__ == '__main__':
